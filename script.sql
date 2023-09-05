1caf9d4d182f8e57c435be8dd9267a71433c5631
lais.uchoa <lais.mendes@fatec.sp.gov.br>
2023-09-05 09:06:49 -0300

    ap08(blocos-anonimos): codigo 2.4.1

--- script.sql
+++ script.sql
@@ -1,6 +1,9 @@
 DO
 $$
+DECLARE
+	codigo INTEGER = 1;
+	nome_completo VARCHAR(200) :- 'João Santos';
+	salario NUMERIC(11,2) := 20.5;
 BEGIN
-RAISE NOTICE '% + % = %', 2, 2, 2 + 2;
+	RAISE NOTICE 'meu código é %, me chamo % e meu salário é %', codigo, nome_completo, salario;
 END;
-$$
